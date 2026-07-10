ac602d7f066bd75162b34540afccfa8cab606f40
Zachary-Kl <zklopp@ivytech.edu>
2026-07-09 20:10:35 -0400

    Add 'open' attribute to Tables class constructor

--- final-project/models.py
+++ final-project/models.py
@@ -1,10 +1,11 @@
 # models.py
 
 class Tables:
-  def __init__(self, tablenum, seats, seatsfull, image):
+  def __init__(self, tablenum, seats, seatsfull, open, image):
     self.tablenum = tablenum
     self.seats = seats
     self.seatsfull = seatsfull
+    self.open = open
     self.image = image
 
 class User:
