fc33b809b68468954fb6256204b51693927bf3e6
Zachary-Kl <zklopp@ivytech.edu>
2026-07-09 20:02:39 -0400

    Add User class with initialization parameters

--- final-project/models.py
+++ final-project/models.py
@@ -7,3 +7,11 @@ class Tables:
     self.seatsfull = seatsfull
     self.image = image
 
+class User:
+  def __init__(self, email, name, number, currenttable, activelyplaying, password):
+    self.email = email
+    self.name = name
+    self.number = number
+    self.currenttable = currenttable
+    self.activelyplaying =a ctivelyplaying
+    self.password = password
