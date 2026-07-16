400f1da2a878c08de657552477998f8c58c7d609
Nichole Bannerman <bannermannichole514@gail.com>
2026-07-16 15:44:59 -0400

    Add waitlist seating logic

--- final-project/database.py
+++ final-project/database.py
@@ -2,3 +2,31 @@ import sqlite3
 
 def get_connection():
     return sqlite3.connect("data.db")
+
+def fill_table_from_waitlist(table, waitlist):
+    """
+    Checks whether a table has open seats.
+    If it does, seats players from the waitlist.
+    """
+
+    seated_players = []
+
+    while table.seatsfull < table.seats:
+
+        player_found = False
+
+        for player in waitlist:
+
+            if player.tablewaitingfor == table.tablenum:
+
+                table.seatsfull += 1
+                seated_players.append(player)
+                waitlist.remove(player)
+
+                player_found = True
+                break
+
+        if not player_found:
+            break
+
+    return seated_players
