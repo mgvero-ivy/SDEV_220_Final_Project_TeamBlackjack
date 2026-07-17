a41d483581d3cfacf113493427c971cfeccd0570
Marko Gvero <mgvero@ivytech.edu>
2026-07-16 19:22:12 -0500

    update app.py

--- final-project/app.py
+++ final-project/app.py
@@ -2,18 +2,31 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route("/")
+#Displays the main page with all currently available tables
+@app.route("/") 
 def index():
-    return render_template("index.html")
 
+    #TO DO this list is temporary and will need to be build with table objects
+    tables = [
+        {"name": "1/3 NL Holdem", "seats": "7/9", "waiting": 0},
+        {"name": "2/5 NL Holdem", "seats": "9/9", "waiting": 3},
+        {"name": "5/10 NL Holdem", "seats": "9/9", "waiting": 1},
+        {"name": "1/2 PLO", "seats": "6/6", "waiting": 4},
+        {"name": "2/5 PLO", "seats": "4/6", "waiting": 0},
+        {"name": "1/3 LIMIT Holdem", "seats": "4/9", "waiting": 0}
+    ]
 
+    return render_template("index.html", tables=tables)
+
+#this shows the player sigh-up page for the chosen table
 @app.route("/join/<path:table_name>", methods=["GET", "POST"])
 def join(table_name):
     if request.method == "POST":
         player_name = request.form["player_name"]
         phone_number = request.form["phone_number"]
 
+
+        #TO DO Save player info, check if selected table has open seat, set table_name to the table number/ID 
         return render_template(
             "confirmation.html",
             table_name=table_name,
