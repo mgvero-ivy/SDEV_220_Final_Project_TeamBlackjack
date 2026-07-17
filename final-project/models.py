aaa230c36fd1f42ba5d1be63f12f686169057dd5
Zachary-Kl <zklopp@ivytech.edu>
2026-07-16 20:13:50 -0400

    Add comments to model class attributes
    Added comments to clarify the purpose of class attributes.

--- final-project/models.py
+++ final-project/models.py
@@ -2,21 +2,22 @@
 
 class Table:
   def __init__(self, tablenum, seats, seatsfull, open):
-    self.tablenum = tablenum
-    self.seats = seats
-    self.seatsfull = seatsfull
+    self.tablenum = tablenum #Number to identify the table
+    self.seats = seats #Total numbers of seats
+    self.seatsfull = seatsfull #Number of seats full
     self.open = open #Whether the table is open or closed
 
 class User:
-  def __init__(self, name, number, currenttable, activelyplaying, password):
-    self.name = name
-    self.number = number
-    self.currenttable = currenttable
-    self.activelyplaying = activelyplaying
-    self.password = password
+  def __init__(self, UID, name, phonenumber, currenttable, activelyplaying, password):
+    self.UID = UID #User ID
+    self.name = name #Name of user
+    self.phonenumber = phonenumber #Phone number used to identify user
+    self.currenttable = currenttable #What table the user is currently at
+    self.activelyplaying = activelyplaying #Whether the player is actively playing
+    self.password = password #Password for the user
 
 class Waitlist:
   def __init__(self, playerid, tablewaitingfor, jointime):
-    self.playerid = playerid
-    self.tablewaitingfor = tablewaitingfor
-    self.jointime = jointime
+    self.playerid = playerid #Player UID
+    self.tablewaitingfor = tablewaitingfor #Which table the player is waiting for
+    self.jointime = jointime #What time the user joined the waitlist
